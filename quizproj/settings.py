a47b0771b3431decce8b8762838aca49091525f6
njoku <njoku@gmail.com>
2025-04-27 02:08:39 +0100

    editted settings

--- quizproj/settings.py
+++ quizproj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q&vxhsx**+gu6ky^c0i!vhz1@-@^-nc##&%c&p9$!d8h)q7b0@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
